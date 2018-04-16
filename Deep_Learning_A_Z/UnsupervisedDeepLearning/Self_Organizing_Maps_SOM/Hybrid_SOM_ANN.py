4361d61f39d9038034b7364d61ea436d66924f54
Jesmond <fayt1989@gmail.com>
2018-04-16 22:50:49 +0800

    Update hybrid model
    Working hybrid model of SOM and ANN to enhanced the prediction made by the SOM

--- Deep_Learning_A_Z/UnsupervisedDeepLearning/Self_Organizing_Maps_SOM/Hybrid_SOM_ANN.py
+++ Deep_Learning_A_Z/UnsupervisedDeepLearning/Self_Organizing_Maps_SOM/Hybrid_SOM_ANN.py
@@ -6,27 +6,45 @@ Python 3.5
 
 Hybrid deep learning model - SOM + ANN
 
-First part is to perform SOM
+First part is to perform SOM. Create additional column to mark those IDs with
+SOM prediction as fraud. Perform fit_transform of the input data before ANN.
 
-Second part is to perform ANN
+Second part is to perform ANN with original data, SOM predictions of fraud,
+a small batch size and epochs is enough for this exercise. Result of the ANN
+with the additional analysis from SOM gives y_pred
+
+Third part is to sort the result. The most likely to be acting fraud has the 
+highest 2nd col in y_pred
 """
-import os
-os.chdir('/home/turbo-octo-potato/Deep_Learning_A_Z/') # change working dir
-from UnsupervisedDeepLearning.Self_Organizing_Maps_SOM import Learning_SOM as SOM
-from SupervisedDeepLearning.Artificial_Neural_Network_ANN import Learning_ANN as ANN
-from sklearn.preprocessing import StandardScaler
 
-SOM.py
+import os
+os.chdir('C:\\Users\\jesmond.lee\\Downloads\\') # change working dir
+from Self_Organizing_Maps_SOM.Learning_SOM import SOM # import class
+from Artificial_Neural_Network_ANN import Learning_ANN as ANN
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+# create an instance and perform SOM
+SOM_instance = SOM() 
+data = SOM_instance.DataPreprocessing()
+trained_SOM = SOM_instance.train_SOM(data[0], data[1])
+result = SOM_instance.Evaluate(data[0], trained_SOM)
+result = SOM.sc.inverse_transform(result)
 
 # create matrix of feature based on the all the data
-customers = SOM.dataset.iloc[:,1:].values
-# create dependent variables
-is_fraud = SOM.np.zero(len(SOM.dataset)) # initialize all cust as no cheat
-for i in range(len(SOM.dataset)):
-    if SOM.dataset.iloc[i,0] in SOM.frauds: # row i, column 0 is cust ID is in SOM fraud result
+customers = data[2].iloc[:,1:].values
+# create dependent variables. Pretend all cust didn't cheat. Replace index to 1 for
+# all those who is predicted as cheats by SOM
+is_fraud = np.zeros(len(data[2])) # initialize all cust as no cheat
+for i in range(len(data[2])):
+    if data[2].iloc[i,0] in result: # row i, column 0 is cust ID is in SOM fraud result
         is_fraud[i] = 1 # 1=approved
         
-sc = StandardScaler()
-customers = sc.fit_transform(customers)
+customers = SOM.sc.fit_transform(customers)
 classifier = ANN.create_ANN(15, 6, 6, 1)
-ANN.train_ANN(classifier)
+classifier.fit(customers, is_fraud, batch_size = 1, epochs =3)
+y_pred = classifier.predict(customers) # probability
+# 1st col cust ID, 2nd col predicted probability, horizontal concatenation
+y_pred = ANN.np.concatenate((data[2].iloc[:,0:1].values, y_pred), axis = 1) 
+# sort array based on 2nd col
+y_pred = y_pred[y_pred[:,1].argsort()]
